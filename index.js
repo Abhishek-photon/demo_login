9d05162e35cf88a7aaefb6a611d91b6cf91ef340
Intrinsic <abhishek.maletha2000@gmail.com>
2021-01-18 23:05:00 +0530

    phone auth done

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
+window.onload = function() {
+  render();
+};
+
+var ph = 0;
+function render(){
+  window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container', {
+    'size': 'invisible',
+    'callback': (response) => {
+      // reCAPTCHA solved, allow signInWithPhoneNumber.
+      onSignInSubmit();
+    }
+  }); 
+  recaptchaVerifier.render().then((widgetId) => {
+    window.recaptchaWidgetId = widgetId;
+  });
+}
+
+
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       // User is signed in.
       document.getElementById("user_div").style.display = "block";
       document.getElementById("login_div").style.display = "none";
+      document.getElementById("otp").style.display = "none";
 
       var user = firebase.auth().currentUser;
 
@@ -10,11 +30,25 @@ firebase.auth().onAuthStateChanged(function(user) {
         var email_id=user.email;
         var txt = `Welcome ${email_id}`;
         document.getElementById("user_para").innerHTML = txt ;
-    }
-    } else {
-      // No user is signed in.
-      document.getElementById("login_div").style.display = "block";
+      }
+    } 
+    
+    else {
       document.getElementById("user_div").style.display = "none";
+      if(!ph){
+        document.getElementById("otp").style.display = "block";
+        document.getElementById("login_div").style.display = "block";
+      }
+
+      else{
+        document.getElementById("otp").style.display = "block";
+        document.getElementById("login_div").style.display = "none";
+        
+
+      }
+      // No user is signed in.
+      
+      
     }
   });
 
@@ -34,6 +68,39 @@ function login(){
     var errorMessage = error.message;
     window.alert(`Error : ${errorMessage} `);
     });
+}
+
+function phoneAuth() {
+  
+  const phoneNumber = document.getElementById('number').value;
+  const appVerifier = window.recaptchaVerifier;
+firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
+    .then((confirmationResult) => {
+      // SMS sent. Prompt user to type the code from the message, then sign the
+      // user in with confirmationResult.confirm(code).
+      window.confirmationResult = confirmationResult;
+      ph=1;
+      window.alert("OTP sent");
+      // ...
+    }).catch((error) => {
+      // Error; SMS not sent
+      // ...
+      window.alert(error.message);
+    }); 
+}
+
+function codeVerify() {
+  const code = document.getElementById('verification').value;
+  confirmationResult.confirm(code).then((result) => {
+    // User signed in successfully.
+    window.alert("success");
+    const user = result.user;
+    // ...
+  }).catch((error) => {
+    // User couldn't sign in (bad verification code?)
+    // ...
+    window.alert(error.message);
+  });
 }
 
 
